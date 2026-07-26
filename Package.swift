814fef48ea162ef7ff82a31617d4712d10d015c1
Galactus-byte <Benjamina195@Outlook.com>
2026-07-26 21:02:13 +0000

    Changed

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "BenjiJailbreak",
+    platforms: [.iOS(.v14)],
+    products: [
+        .executable(name: "BenjiJailbreak", targets: ["BenjiJailbreak"])
+    ],
+    targets: [
+        .executableTarget(
+            name: "BenjiJailbreak",
+            path: "Sources",
+            sources: ["BenjiJailbreak.swift"],
+            linkerSettings: [
+                .linkedFramework("UIKit"),
+                .linkedFramework("SwiftUI"),
+                .linkedFramework("Foundation"),
+                .linkedFramework("IOKit")
+            ]
+        )
+    ]
+)
